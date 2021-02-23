2cd77294a59640218904655e6980ae0695b907cc
weslleyg <wesley.dipaulatorres@hotmail.com>
2021-02-23 19:38:55 -0300

    criando usuarios

---
+++ src/routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import { UserControler } from './controllers/UserController';
+
+const router = Router();
+
+const userController = new UserControler();
+
+router.post('/users', userController.create);
+
+export { router }
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
 import 'reflect-metadata'
 import express = require('express');
 import "./database"
+import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    return res.json({message: 'Hello World - NLW'})
-})
+app.use(router);
 
 app.listen(3333, () => console.log('Server is running'));
